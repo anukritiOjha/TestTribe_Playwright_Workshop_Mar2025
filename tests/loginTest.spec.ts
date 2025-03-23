920ae91fd852d706d85902a84bdffaf98beb4c56
Anukriti Ojha <anukritiece@gmail.com>
2025-03-23 17:22:35 +0530

    updating loginTest with my changes

--- tests/loginTest.spec.ts
+++ tests/loginTest.spec.ts
@@ -5,6 +5,22 @@ test('OrangeHRM Login Test', async ({ page }) => {
     await page.locator('[name="username"]').fill('Admin');
     await page.locator('//input[@placeholder="Password"]').fill('admin123');
     await page.locator('[type="submit"]').click();
+
+    //assertion
     await expect(page.locator('.oxd-topbar-header-breadcrumb-module')).toBeVisible();
-    await expect(page.locator('.oxd-topbar-header-breadcrumb-module')).toHaveText('Dashboard');
-})
+    //await expect(page.locator('.oxd-topbar-header-breadcrumb-module')).toHaveText('Dashboard');
+    await page.pause();
+    await page.screenshot({ path: `screenshot.png` });
+
+});
+
+//actual code by Vignesh
+// import { test, expect } from '@playwright/test'
+// test('OrangeHRM Login Test', async ({ page }) => {
+//     await page.goto("https://opensource-demo.orangehrmlive.com/");
+//     await page.locator('[name="username"]').fill('Admin');
+//     await page.locator('//input[@placeholder="Password"]').fill('admin123');
+//     await page.locator('[type="submit"]').click();
+//     await expect(page.locator('.oxd-topbar-header-breadcrumb-module')).toBeVisible();
+//     await expect(page.locator('.oxd-topbar-header-breadcrumb-module')).toHaveText('Dashboard');
+// })
